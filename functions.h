695838390cf280cb1e3d1ff19e4920eb14128a15
liz0k <izolda257as@gmail.com>
2022-03-16 20:57:03 +0300

    server vers.2 kuzmich djakonov vojtovich

---
+++ functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONSFORSERVER_H
+#define FUNCTIONSFORSERVER_H
+#include <QString>
+
+QByteArray parsing(QString data_from_client);
+
+QByteArray auth(QString login, QString pass);
+QByteArray reg(QString login, QString pass, QString mail);
+
+#endif // FUNCTIONSFORSERVER_H
