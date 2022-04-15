d9a6f0097c371c63f033ec0f42f749d637d12826
Pavel <pashadyakonov27082003@gmail.com>
2022-04-15 20:11:08 +0900

    little refactor

---
+++ server/functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONSFORSERVER_H
+#define FUNCTIONSFORSERVER_H
+
+#include "db.h"
+
+#include <QString>
+#include <QStringList>
+
+QByteArray parsing(QString data_from_client);
+
+QByteArray auth(QString login, QString pass);
+QByteArray reg(QString login, QString pass, QString mail);
+
+#endif // FUNCTIONSFORSERVER_H
