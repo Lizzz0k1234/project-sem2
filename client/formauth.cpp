4a13f25270ffb0aabdcb00dd6f62b5ecc71b9685
Pavel <pashadyakonov27082003@gmail.com>
2022-04-19 01:49:55 +0900

    client

--- client/formauth.cpp
+++ client/formauth.cpp
@@ -1,6 +1,6 @@
 #include "formauth.h"
 #include "ui_formauth.h"
-
+#include <QMessageBox>
 FormAuth::FormAuth(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::FormAuth)
@@ -30,26 +30,79 @@ void FormAuth::on_pushButton_cancel_clicked()
     ui->pushButton_reg->setVisible(true);
 }
 
-
-void FormAuth::on_pushButton_ok_clicked()
+void FormAuth::check_auth()
 {
     QString log = ui -> lineEdit_log -> text();
     QString pass = ui -> lineEdit_pass -> text();
+    bool g = true;
     if (ui -> lineEdit_email -> isVisible())
     {
         //reg
         QString email = ui -> lineEdit_email -> text();
-        reg(log, pass, email);
+        if (email!="") reg(log, pass, email);
+        else
+        {
+            QMessageBox temp;
+            temp.setText("Please, write your email");
+            temp.exec();
+            g = false;
+
+        }
     }
 
     else
     {
         //auth
+        g = true;
         auth(log,pass);
+
     }
 
-    emit logged_in(log);
-    //goto MainWindow();
-    hide();
+    if (g==true)
+    {
+        emit logged_in(log);
+        //goto MainWindow();
+        hide();
+    }
+}
+void FormAuth::on_pushButton_ok_clicked()
+{
+    QString log = ui -> lineEdit_log -> text();
+    QString pass = ui -> lineEdit_pass -> text();
+
+    if (pass!="")
+    {
+        if (log!="") check_auth();
+        else
+        {
+            QMessageBox temp;
+            temp.setText("Please, Fill in all the fields");
+            temp.exec();
+        }
+    }
+    else
+    {
+        QMessageBox temp;
+        temp.setText("Please, Fill in all the fields");
+        temp.exec();
+    }
+}
+
+
+void FormAuth::on_lineEdit_pass_returnPressed()
+{
+    on_pushButton_ok_clicked();
+}
+
+
+void FormAuth::on_lineEdit_email_returnPressed()
+{
+    on_pushButton_ok_clicked();
+}
+
+
+void FormAuth::on_lineEdit_log_returnPressed()
+{
+    on_pushButton_ok_clicked();
 }
 
--- client/formauth.h
+++ client/formauth.h
@@ -22,6 +22,13 @@ private slots:
     void on_pushButton_cancel_clicked();
 
     void on_pushButton_ok_clicked();
+    void check_auth();
+    void on_lineEdit_pass_returnPressed();
+
+    void on_lineEdit_email_returnPressed();
+
+    void on_lineEdit_log_returnPressed();
+
 signals:
     void logged_in(QString log);
 private:
